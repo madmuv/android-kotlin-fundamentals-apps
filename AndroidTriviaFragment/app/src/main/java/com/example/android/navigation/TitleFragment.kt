468541970f21cc57f563481c7abf495555d9f04f
madmuv <d.kritaisong@gmail.com>
2022-05-22 20:21:49 +0700

    - configed kotlin version to 1.6.21

--- AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTriviaFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- package com.example.android.navigation
+package com.example.android.navigation
 
 
 import android.os.Bundle
@@ -23,26 +23,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+    lateinit var binding : FragmentTitleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_title, container, false
+        )
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.playButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+    }
 }
